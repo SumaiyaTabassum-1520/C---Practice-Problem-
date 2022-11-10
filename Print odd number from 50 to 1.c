828f028b0502096b03398f56afc14eb4e3746590
SumaiyaTabassum-1520 <105984307+SumaiyaTabassum-1520@users.noreply.github.com>
2022-11-10 10:43:26 +0600

    Print odd number from 1 to 50 in ascending order

---
+++ Print odd number from 50 to 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+
+    for(i=50 ; i>=1 ; i--)
+    {
+        if(i%2==1)
+        {
+            printf("%d\n",i);
+        }
+
+
+    }
+return 0;
+}
+
+
