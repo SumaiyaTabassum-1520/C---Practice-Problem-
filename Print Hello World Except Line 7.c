f2249791143934f56f2c84a91ba2559094b990fe
SumaiyaTabassum-1520 <105984307+SumaiyaTabassum-1520@users.noreply.github.com>
2022-11-10 10:43:02 +0600

    Printing hello world skipping line number 7

---
+++ Print Hello World Except Line 7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1 ; i<=10 ; i++)
+    {
+        if(i==7)
+        {
+            continue;
+        }
+        printf("%d Hello World\n",i);
+    }
+}
+
